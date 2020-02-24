7070f175db84fd39a066b01b477289f23e7358ad
Ansh Puvvada <43775333+crusader2000@users.noreply.github.com>
2020-02-24 14:39:17 +0530

    Update processor_tb.v

--- processor_tb.v
+++ processor_tb.v
@@ -1,23 +1,25 @@
 `timescale 1ns / 1ps
-
 module processor_tb;
 
 	// Inputs
 	reg clk; 
 	wire [31:0] pc;
 	wire [31:0] instr;
+	wire [31:0] ALU_Out;
 
 	// Instantiate the Unit Under Test (UUT)
 	processor uut (
 		.clk(clk),
 		.instr(instr),
-		.pc(pc)
+		.pc(pc),
+		.ALU_Out(ALU_Out)
 	);
-
+	initial #200 $finish;
 	initial begin
 		// Initialize Inputs
-		clk = 0;
-	end
+		#100;
+		clk = 1;
+	end 
 	
 	always begin
 	#5 clk=~clk;
